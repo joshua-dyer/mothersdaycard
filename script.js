30d96b41b3421f20c36bcb1085d71bab331ff0e6
joshua-dyer <145073496+joshua-dyer@users.noreply.github.com>
2025-05-11 09:27:12 -0500

    Add files via upload
    Updated comments and rearranged my JS Variables to be more uniform

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const card = document.getElementById('myCard');
+const cardContainer = document.querySelector('.card-container');
+const fadeInText = document.getElementById('fade-in-text');
+
 
 card.addEventListener('click', function() {
     card.classList.toggle('is-flipped');
 });
 
-const cardContainer = document.querySelector('.card-container');
-const fadeInText = document.getElementById('fade-in-text');
-
 cardContainer.addEventListener('click', () => {
-  // Add a class to trigger the flip (you likely have this already)
+  // Add a class to trigger the flip
   cardContainer.classList.toggle('flipped');
 
   // Wait for a short delay (after the flip animation starts) and then trigger the fade-in
   setTimeout(() => {
     fadeInText.classList.add('start-fade');
-  }, 500); // Adjust this delay (in milliseconds) to match your flip animation duration
+  }, 500); // Adjust this delay to match flip animation duration
 });
